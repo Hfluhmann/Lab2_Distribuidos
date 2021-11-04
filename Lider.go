67239c6c1afcf1d73a36bb49275aef763f1411a2
Hans <hfluhmann@gmail.com>
2021-11-03 22:06:36 -0300

    finalizado analizis del lider fase 1 del juego (falta agregar valores de jugadores)

--- Lider.go
+++ Lider.go
@@ -7,12 +7,12 @@ import (
 )
 
 func kill_player(player_number int) {
-	//le dice al jugador numero player_number que se murio y se debe desconectar
-	//le dice al pozo que el jugador numero player_number murio y que actualice el pozo y el txt
+	//POR HACER le dice al jugador numero player_number que se murio y se debe desconectar
+	//POR HACER le dice al pozo que el jugador numero player_number murio y que actualice el pozo y el txt
 	return
 }
 
-//Jugar primera fase para un bot
+//Compara los puntajes seleccionados de un participante con el lider en el primer juego, y decide si eliminarlo
 func comparar(valor_jugador int, valor_lider int) bool {
 	if valor_jugador < valor_lider {
 
@@ -27,46 +27,54 @@ func comparar(valor_jugador int, valor_lider int) bool {
 
 }
 
-func final_F1() {
-	var a [2]int
-
-	fmt.Println(a)
-	fmt.Println(a[0])
-	fmt.Println(a[1])
-	return
-}
+//Analizis del Lider sobre el primer juego
 func Jugar_F1() {
+	//puntaje acumulado que lleva un jugador en esta ronda (solo suma si sigue vivo)
 	puntajes := [16]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	//0 si el jugador de la posicion correspondiente esta vivo, 1 si esta eliminado
 	eliminados := [16]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-	//generar seed cambiantes para los random
+
+	//for que lleva cada ronda del primer juego
 	for i := 0; i < 4; i++ {
 
+		//generar seed cambiantes para los random
 		s1 := rand.NewSource(time.Now().UnixNano() * 100)
 		r1 := rand.New(s1)
+		// El lider selecciona un valor entre 6 y 10
 		valor_lider := r1.Intn(5) + 6
 
 		fmt.Println(valor_lider)
 		for j := 0; j < 16; j++ {
 			if eliminados[j] == 0 {
-				//recibir numero de cada jugador
-				var movimiento bool = comparar(7, valor_lider) //se esta usando un valor generico 7 como valor del juagdor
+				//POR HACER recibir numero de cada jugador
+				var movimiento bool = comparar(7, valor_lider) //POR HACER se esta usando un valor generico 7 como valor del juagdor
 				if movimiento {
-					puntajes[j] += 7 //se suma el puntaje que puso el jugador
+					puntajes[j] += 7 //POR HACER se suma el puntaje que puso el jugador
 				} else {
-					kill_player(j)
+					kill_player(j + 1)
 					eliminados[j] = 1
 				}
 			}
 		}
 	}
+
+	for j := 0; j < 16; j++ {
+		if eliminados[j] == 0 {
+			if puntajes[j] < 21 {
+				kill_player(j + 1)
+				eliminados[j] = 1
+			}
+		}
+	}
+
 	fmt.Println(puntajes)
 	fmt.Println(eliminados)
 	return
 
 }
 
-//Jugar segunda fase para un bot
-func Fase2(player_number int64) int {
+//Analizis del Lider sobre el segundo juego
+func Jugar_F2(player_number int64) int {
 
 	s2 := rand.NewSource(time.Now().UnixNano() * player_number)
 	r2 := rand.New(s2)
@@ -76,7 +84,7 @@ func Fase2(player_number int64) int {
 }
 
 //Jugar tercera fase para un bot
-func Fase3(player_number int64) int {
+func Jugar_F3(player_number int64) int {
 
 	s3 := rand.NewSource(time.Now().UnixNano() * player_number)
 	r3 := rand.New(s3)

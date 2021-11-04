bd7eae5c577104b3c818fb17c3b252ad51325632
Hans <hfluhmann@gmail.com>
2021-11-03 21:51:35 -0300

    cambiado jugador en base a los requerimientos de la tarea

--- Jugador.go
+++ Jugador.go
@@ -1,45 +1,51 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
 	"math/rand"
 	"time"
 )
 
 //Jugar primera fase para un bot
-func Fase1() {
-	s1 := rand.NewSource(time.Now().UnixNano())
+func Fase1(player_number int64) int {
+
+	//generar seed cambiantes para los random
+	s1 := rand.NewSource(time.Now().UnixNano() * player_number)
 	r1 := rand.New(s1)
 
-	var suma int = 0
-
-	for j := 0; j < 4; j++ {
-
-		//generar seed cambiantes para los random
-
-		//Generar numero entre el (0 y el 9) + 1
-		var num_jefe int = 7 //numero generico para considerar que elige el Jefe
-		var actual int = r1.Intn(10) + 1
-
-		fmt.Println(suma, actual)
-
-		if actual < num_jefe {
-			// SOBREVIVE LA ELECCION
-			suma += actual
-		} else {
-			// NO SOBREVIVE
-		}
-
-		if suma < 21 {
-			// NO SOBREVIVE
-		}
-
-	}
+	return r1.Intn(10) + 1
 
 }
 
+//Jugar segunda fase para un bot
+func Fase2(player_number int64) int {
+
+	s2 := rand.NewSource(time.Now().UnixNano() * player_number)
+	r2 := rand.New(s2)
+
+	return r2.Intn(4) + 1
+
+	l := list.New() // Initialize an empty list
+	l.PushFront(10)
+	fmt.Println(l.Front()) // &{{0x43e280 0x43e280 <nil> <nil>} 0}
+}
+
+//Jugar tercera fase para un bot
+func Fase3(player_number int64) int {
+
+	s3 := rand.NewSource(time.Now().UnixNano() * player_number)
+	r3 := rand.New(s3)
+
+	return r3.Intn(10) + 1
+
+}
+
+func delete() {
+
+}
 func main() {
 
-	Fase1()
+	Fase2(1)
 
 }
